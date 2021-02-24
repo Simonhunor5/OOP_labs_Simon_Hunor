4b8efdf0ba366541d1aff8957cabf71dd6629fb1
Simonhunor5 <79147758+Simonhunor5@users.noreply.github.com>
2021-02-24 15:14:42 +0200

    Feladat 4
    Update

--- Labor1/FourthProject/src/com/company/Main.java
+++ Labor1/FourthProject/src/com/company/Main.java
@@ -1,15 +1,33 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        String[] tomb;
-        String nev2 = "Kerekes Bálint Ádám József";
-        tomb = nev2.split(" ");
-
-        for(int i = 0; i < tomb.length; i++) {
-            System.out.printf("%c", tomb[i].charAt(0));
-        }
+//
+        System.out.println(createMonogram("Simon Hunor"));
     }
+
+    /**
+     *
+     * @param name -> ennek fogjuk meghatarozni a monogramjat
+     * @return -> a monogram
+     */
+    public static String createMonogram(String name) {
+        //name tartalma: Antal Jozsef
+        String[] tomb;
+        tomb = name.split(" ");
+        //tomb[0] = Antal
+        //tomb[1] = Jozsef
+        String monogram = "";
+//        for(int i = 0; i < tomb.length; ++i) {
+//            monogram += tomb[i].charAt(0);
+//        }
+        for (String s : tomb) {
+            monogram += s.charAt(0);
+        }
+
+        return monogram;
+    }
+
+
+
 }
