293a6e559dae7193cc6b26e8f9c049329e8e0e63
Simonhunor5 <79147758+Simonhunor5@users.noreply.github.com>
2021-02-25 23:13:00 +0200

    Test

--- Extra/src/com/company/Main.java
+++ Extra/src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
 		Car car = new Car("Ford", "Fiesta", 2, true, 2002);
 		System.out.println(car);
 
+		Car car1 = new Car("Nissan", "Note", 2, true, 2009);
+		System.out.println(car1);
+
 
 	}
 }
