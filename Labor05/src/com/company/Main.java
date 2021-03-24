e09e350567213b6be567efee2a10cf06d77850cc
Simonhunor5 <79147758+Simonhunor5@users.noreply.github.com>
2021-03-24 16:11:20 +0200

    Without testing

--- Labor05/src/com/company/Main.java
+++ Labor05/src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Bank bank = new Bank("OTP");
+        bank.addCustomer(new Customer("JHON", "DOE"));
+        bank.addCustomer(new Customer("MARY", "WHITE"));
+
+        for(int i = 0; i < bank.numCustomer(); i++)
+        {
+            
+        }
+
     }
 }
